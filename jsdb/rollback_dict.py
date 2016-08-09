f70c2101f4234ab08cdea7741d40e1ac7f8ba731
Tal Wrii <talwrii@googlemail.com>
2016-08-09 12:56:00 -0500

    rollback_dict -- Various
    Changes to descendants should still be commited when the root is
    commited
    
    Only allow committing to take place at the top level (proactive monster-barring)

--- jsdb/rollback_dict.py
+++ jsdb/rollback_dict.py
@@ -17,26 +17,33 @@ class RollbackableMixin(object):
         if stored == DELETED:
             raise KeyError(key)
 
-        if not isinstance(stored, JSON_TYPES):
-            print repr(stored)
-            raise ValueError(stored)
-        if isinstance(stored, dict):
-            result = RollbackDict(stored)
-            self[key] = result
-            return result
-        elif isinstance(stored, list):
-            result = RollbackList(stored)
-            self[key] = result
-            return result
-        else:
-            return stored
+        return self._rollback_wrap(stored)
 
+    def _rollback_wrap(self, value):
+        "Make the value rollbackable"
+
+        if not isinstance(value, JSON_TYPES):
+            raise ValueError(value)
+        elif isinstance(value, collections.MutableMapping):
+            if isinstance(value, RollbackDict):
+                return value
+            else:
+                return RollbackDict(value, parent=self)
+        elif isinstance(value, collections.MutableSequence):
+            if isinstance(value, RollbackList):
+                return value
+            else:
+                return RollbackList(value, parent=self)
+        else:
+            return value
 
 class RollbackDict(RollbackableMixin, collections.MutableMapping):
     "A proxy for changing an underlying data structure that commit and rollback"
-    def __init__(self, prototype):
-        self._prototype = prototype
+    def __init__(self, underlying, parent=None):
+        self._underlying = underlying
         self._updates = {}
+        self._parent = parent
+        self._changed_descendents = []
 
     def _items(self):
         return self.items()
@@ -45,17 +52,25 @@ class RollbackDict(RollbackableMixin, collections.MutableMapping):
         if key in self._updates:
             return self._updates[key]
         else:
-            return self._prototype[key]
+            return self._underlying[key]
 
     def __setitem__(self, key, value):
-        self._updates[key] = value
+        if self._parent:
+            self._parent._record_changed(self) # pylint: disable=protected-access
+        self._updates[key] = self._rollback_wrap(value)
+
+    def _record_changed(self, item):
+        if self._parent:
+            self._parent._record_changed(item) # pylint: disable=protected-access
+        else:
+            self._changed_descendents.append(item)
 
     def __iter__(self):
         for key, value in self._updates.items():
             if value != DELETED:
                 yield key
 
-        for key in self._prototype:
+        for key in self._underlying:
             if key not in self._updates:
                 yield key
 
@@ -63,18 +78,32 @@ class RollbackDict(RollbackableMixin, collections.MutableMapping):
         self._updates[key] = DELETED
 
     def __len__(self):
-        additions = len([x for x in self._updates.values() if x not in self._prototype])
+        additions = len([x for x in self._updates.keys() if x not in self._underlying])
         deletions = len([x for x in self._updates.values() if x == DELETED])
-        return len(self._prototype) + additions - deletions
+        return len(self._underlying) + additions - deletions
 
     def commit(self):
+        if self._parent is not None:
+            raise Exception('Can only commit at top level')
+        self._commit()
+
+    def _commit(self):
+        for desc in self._changed_descendents:
+            desc._commit()
+
         for k, v in self._updates.items():
-            self._prototype[k] = v
+            if v == DELETED:
+                del self._underlying[k]
+            else:
+                if isinstance(v, RollbackableMixin):
+                    v._commit()
+                    self._underlying[k] = v._underlying
+                else:
+                    self._underlying[k] = v
         self._updates.clear()
 
     def python_copy(self):
         return python_copy.dict_copy(self)
-
 
 class RollbackList(RollbackableMixin, collections.MutableSequence):
     """A proxy for changing an underlying data structure that supports commit and rollback
@@ -82,9 +111,10 @@ class RollbackList(RollbackableMixin, collections.MutableSequence):
     This works by creating a complete clone of the underlying list. Simplifying slicing etc
     """
 
-    def __init__(self, underlying):
+    def __init__(self, underlying, parent=None):
         self._underlying = underlying
         self._new = None
+        self._parent = parent
 
     def insert(self, index, obj):
         self._ensure_copied()
@@ -105,27 +135,37 @@ class RollbackList(RollbackableMixin, collections.MutableSequence):
         return self._new[key]
 
     def _ensure_copied(self):
-        if self._new is None:
-            self._new = copy.copy(self._underlying)
+        if self._parent:
+            self._parent._record_changed(self)
+        if not self._is_updated():
+            self._new = list(self._underlying)
 
     def __delitem__(self, key):
         self._ensure_copied()
         del self._new[key]
 
     def __iter__(self):
-        if self._new is not None:
+        if self._is_updated():
             return iter(self._new)
         else:
             return iter(self._underlying)
 
     def __len__(self):
-        if self._new is not None:
+        if self._is_updated():
             return len(self._new)
         else:
             return len(self._underlying)
 
-    def commit(self):
-        self._underlying[:] = self._new
+    def _is_updated(self):
+        return self._new is not None
+
+    def _record_changed(self, item):
+        if self._parent:
+            self._parent._record_changed(item)
+
+    def _commit(self):
+        if self._is_updated():
+            self._underlying[:] = self._new
         self._new = None
 
 class TestRollback(unittest.TestCase):
@@ -139,7 +179,7 @@ class TestRollback(unittest.TestCase):
         self.assertEquals(under['a'], 1)
         self.assertEquals(roll['b'], 2)
         self.assertTrue('b' not in under)
-        roll.commit()
+        roll._commit()
         self.assertEquals(under, dict(a=4, b=2))
 
     def test_rollback_list(self):
@@ -150,7 +190,7 @@ class TestRollback(unittest.TestCase):
         self.assertEquals(list(lst), [1])
         self.assertEquals(len(lst), 1)
         self.assertEquals(lst[0], 1)
-        lst.commit()
+        lst._commit()
         self.assertEquals(under, [1])
         self.assertEquals(list(lst), [1])
 
@@ -163,7 +203,7 @@ class TestRollback(unittest.TestCase):
         self.assertEquals(roll['a']['b'], 2)
         proxy = roll['a']
 
-        roll.commit()
+        roll._commit()
 
         self.assertEquals(under['a']['b'], 2)
         self.assertEquals(roll['a']['b'], 2)
@@ -192,8 +232,33 @@ class TestRollback(unittest.TestCase):
         roll.insert(1, 1)
         self.assertFalse(under)
         self.assertTrue(list(roll) == [1])
-        roll.commit()
+        roll._commit()
         self.assertTrue(list(under) == [1])
+
+    def test_subcommit(self):
+        under = dict()
+        d = RollbackDict(under)
+
+        d['a'] = {}
+        d._commit()
+        d['a']['b'] = 'dirty'
+        d._commit()
+        self.assertEquals(under['a']['b'], 'dirty')
+
+    def test_subcommit_list(self):
+        under = dict()
+        d = RollbackDict(under)
+        d['a'] = []
+        d._commit()
+        d['a'].insert(0, [])
+        d._commit()
+        d['a'][0].insert(0, [])
+        d._commit()
+        d['a'][0][0].insert(0, 17)
+        d._commit()
+        self.assertEquals(under['a'][0][0][0], 17)
+
+
 
 
 if __name__ == '__main__':
